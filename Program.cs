069f66ea7f19d434a1d039b9bc3fa419503f8231
Thomas <tomn1345@gmail.com>
2024-12-10 13:55:07 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Backtesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
