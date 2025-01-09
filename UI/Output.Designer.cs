a2721e7027a54a27d26073a102d2ebcfe557fa28
Thomas <tomn1345@gmail.com>
2025-01-09 09:49:57 +0100

    To update

--- UI/Output.Designer.cs
+++ UI/Output.Designer.cs
@@ -35,6 +35,7 @@
             panel1 = new Panel();
             label1 = new Label();
             splitContainer1 = new SplitContainer();
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
@@ -48,10 +49,11 @@
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
-            dataGridView1.Location = new Point(0, 3);
+            dataGridView1.Location = new Point(0, 2);
+            dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(403, 871);
+            dataGridView1.Size = new Size(516, 523);
             dataGridView1.TabIndex = 0;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
@@ -75,25 +77,29 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(formsPlot1);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(57, 33);
+            panel1.Location = new Point(2, 17);
+            panel1.Margin = new Padding(2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(658, 346);
+            panel1.Size = new Size(641, 566);
             panel1.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial Black", 15F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(3, 1);
+            label1.Location = new Point(2, 1);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(204, 42);
+            label1.Size = new Size(136, 28);
             label1.TabIndex = 0;
             label1.Text = "Indicators :";
             // 
             // splitContainer1
             // 
-            splitContainer1.Location = new Point(26, 56);
+            splitContainer1.Location = new Point(18, 34);
+            splitContainer1.Margin = new Padding(2);
             splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -103,16 +109,26 @@
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(panel1);
-            splitContainer1.Size = new Size(2883, 1159);
-            splitContainer1.SplitterDistance = 961;
+            splitContainer1.Size = new Size(2018, 695);
+            splitContainer1.SplitterDistance = 672;
+            splitContainer1.SplitterWidth = 3;
             splitContainer1.TabIndex = 2;
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.DisplayScale = 1F;
+            formsPlot1.Location = new Point(44, 54);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(544, 287);
+            formsPlot1.TabIndex = 1;
             // 
             // Output
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2953, 1243);
+            ClientSize = new Size(1347, 637);
             Controls.Add(splitContainer1);
+            Margin = new Padding(2);
             Name = "Output";
             Text = "TradeSoft - Result";
             Load += Output_Load;
@@ -135,5 +151,6 @@
         private Panel panel1;
         private Label label1;
         private SplitContainer splitContainer1;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
     }
 }
